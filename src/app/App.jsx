68df497da138f3f05ece5aa416d2db56713ae99c
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-07-17 22:05:32 +0500

    alias configured and updated

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <></>;
+  return <div>App</div>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app/App.jsx";
+import { StrictMode } from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/routing/router.jsx";
 import "./app/styles/globals.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
